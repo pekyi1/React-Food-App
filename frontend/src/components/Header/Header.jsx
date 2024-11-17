6e6422233d414cfa56be81f7de732b48957e1f11
Fred Pekyi <fredpekyi01@gmail.com>
2024-11-17 20:15:46 +0000

    initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, provident dignissimos atque et eligendi temporibus ab cupiditate velit aliquam recusandae voluptate ipsum soluta praesentium id doloribus odio reprehenderit! Delectus, quaerat!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
